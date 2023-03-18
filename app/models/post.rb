3e22f08aad82e869af05524468605152af37165c
Joseph <wamiti8711@gmail.com>
2023-03-18 11:32:38 -0400

    active record validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :sufficiently_clickbait_y_title
+  
+    def sufficiently_clickbait_y_title
+      if title.present? && !title.include?("Won't Believe") && !title.include?("Secret") && !title.match?(/Top \d+/) && !title.include?("Guess")
+        errors.add(:title, "must be sufficiently clickbait-y")
+      end
+    end
+  end
